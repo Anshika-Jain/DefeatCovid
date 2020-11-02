70ea993d6ccd0b488f100f6a0f67fb3666e3ee92
tushar-bht <tusharbhtt67@gmail.com>
2020-11-03 00:09:24 +0530

    improved nav-bar

--- front-end/src/components/mainHeader.js
+++ front-end/src/components/mainHeader.js
@@ -1,27 +1,44 @@
 import React, { useContext } from "react";
 import Navbar from "react-bootstrap/Navbar";
+
 import Nav from "react-bootstrap/Nav";
 import { AuthContext } from "./context/auth-context";
 
 export default function MainHeader() {
   const auth = useContext(AuthContext);
   return (
-    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-      <Navbar.Brand href="/">HOME</Navbar.Brand>
+    <Navbar
+      className="nav-bar"
+      collapseOnSelect
+      expand="lg"
+      bg="dark"
+      variant="dark"
+    >
+      <Navbar.Brand className="nav-brand" href="/">
+        HOME
+      </Navbar.Brand>
       <Navbar.Toggle aria-controls="responsive-navbar-nav" />
       <Navbar.Collapse id="responsive-navbar-nav">
         <Nav className="mr-auto">
           {auth.isLoggedIn && (
-            <Nav.Link href="/post/createPost">Create Post</Nav.Link>
+            <Nav.Link className="navbar-link" href="/post/createPost">
+              Create Post
+            </Nav.Link>
           )}
         </Nav>
         <Nav>
           {!auth.isLoggedIn && (
-            <Nav.Link href="/authenticate">Login/signup</Nav.Link>
+            <Nav.Link className="navbar-link" href="/authenticate">
+              Login/signup
+            </Nav.Link>
           )}
           {auth.isLoggedIn && (
             <span onClick={auth.logout}>
-              <Nav.Link eventKey={2} href="/authenticate">
+              <Nav.Link
+                className="navbar-link"
+                eventKey={2}
+                href="/authenticate"
+              >
                 Logout
               </Nav.Link>
             </span>
