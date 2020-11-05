d9f098ba6ff0ec1ddf678fa5374a810392e32279
Anshika Jain <anshikajain604@gmail.com>
2020-11-06 01:29:08 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,40 +1,56 @@
-fetch('https://api.covid19api.com/summary')
-    .then((apidata) => {
-        return apidata.json();
-        // console.log(apidata);
-    })
+  
+    var s;
+    var p;
+    var f;
 
-    .then((actualdata) => {
-
-            console.log(actualdata);
-            //confirm
-            const mydata = actualdata.Countries[100].TotalConfirmed;
-            document.getElementById('conin').innerHTML = mydata;
-
-
-            //recover
-            const recdata = actualdata.Countries[100].TotalRecovered;
-            document.getElementById('revin').innerHTML = recdata;
-
-            //deaths
-            const detdata = actualdata.Countries[100].TotalDeaths;
-            document.getElementById('detin').innerHTML = detdata;
-
-            //global data 
-
-            const globco = actualdata.Global.TotalConfirmed;
-            document.getElementById('globco').innerHTML = globco;
-
-            //global recover
-            const globro = actualdata.Global.TotalRecovered;
-            document.getElementById('globro').innerHTML = globro;
-            //global deaths
-
-            const globdo = actualdata.Global.TotalDeaths;
-            document.getElementById('globdo').innerHTML = globdo;
+function res()
+{
+    
+  
+    s=0;
+    var x=document.getElementById("formtest");
+    var i;
+    var flag;
+    flag=0;
+    for(i=0;i<x.length;i++)
+    {
+        if(parseInt(x.elements[i].value)>5 || parseInt(x.elements[i].value)<1)
+        {
+        alert("enter values from 1 to 5 only in the test");
+        break;
+        flag=1;
         }
+        else
+        s=s+parseInt(x.elements[i].value);
+    }
+    if(flag==0)
+    {
+        p = s/75;
+        f = p*100;
+        document.getElementById("result").innerHTML="Your Mental Well-Being is "+ f +"% in danger.";
+        document.getElementById("testsubmitbtn").style.visibility="hidden";
+        document.getElementById("solnbtn").style.visibility="visible";
+    }
+    
+    if(flag==1)
+    document.getElementById("result").innerHTML="please enter valid data";
+    
 
-    )
-    .catch((error) => {
-        console.log(`the error: ${error}`);
-    });
+}
+
+function soln()
+{
+    document.getElementById("mwb").style.visibility="hidden";
+    document.getElementById("solnbtn").style.visibility="hidden";
+     if(f >=55)
+     {
+        document.getElementById("soln1").style.visibility="visible";
+        document.getElementById("soln1").style.top= "-1550px";
+     }
+
+     else if(f < 55)
+     {
+        document.getElementById("soln2").style.visibility="visible";
+        document.getElementById("soln2").style.top= "-1550px";
+     }
+}
